64ecb0f754314e83ad41601977adcb6182c32b62
galqnikilova-netizen <galqnikilova@gmail.com>
2026-07-16 21:18:41 +0300

    fix: run locale middleware after web session starts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(SetNexusLocale::class);
+        $middleware->appendToGroup('web', SetNexusLocale::class);
         $middleware->append(EnsureNexusIsInstalled::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
